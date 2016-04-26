9259b62b5f2513e60a38f332eb800fb9dc522398
onelangs <onelang@163.com>
2016-04-27 03:11:34 +0800

    第一章完成，但是例子执行不过去。睡觉

--- func_test.py
+++ func_test.py
@@ -1,11 +1,44 @@
-
+#!/usr/bin/python3
+#-*- coding:utf8 _*_
 from selenium import webdriver
+import unittest
 
-brower=webdriver.Firefox()
-
-brower.get('http://localhost:8000')
-
-assert 'Django' in brower.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.brower = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.brower.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+                
+        #老王听说有一个很酷的在线待办事项应用
+        #他去看了这个应用的首页
+        self.brower.get('http://localhost:8000')
+        #他注意到网页的标题盒头部都包含“To-Do”这个词
+        self.assertIn('To-Do',brower.title)
+        sefl.fail('finish the test!')
+        
+        
+        #应用邀请他输入一个待办事项
+        #他在一个文本框中输入了"买一本书"
+        #老王爱看书
+        #他按回车键后，页面更新了
+        #待办事项表格中显示了”1：买一本书“
+        
+        #页面中又显示利一个文本框，可以输入其他待办事项
+        #他又输入了”看书后思考“
+        
+        #页面再次更新，他的清单中显示了两个待办事项
+        
+        #老王想知道这个网站是否会记住他的清单
+        #他看到利网站为他生成了一个唯一的URL
+        #而且页面中有一些文字解说这个功能
+        #他访问这个URL，发现他的待办事项还在。
+        #老王很满意，去吃饭利。
+        self.brower.quite()
+    if __name__ == '__main__':
+        unittest.main()
 
 
 
