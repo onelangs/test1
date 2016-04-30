d1275b6443431f054076f0c33f8cd3909e2ed595
onelangs <onelang@163.com>
2016-04-30 13:46:02 +0800

    功能测试重构

--- func_test.py
+++ func_test.py
@@ -13,6 +13,13 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.brower.quit()
         
+    def check_for_row_in_list_table(self,row_text):
+        table = self.brower.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+        
+        
+        
     def test_can_start_a_list_and_retrieve_it_later(self):
              
         #老王听说有一个很酷的在线待办事项应用
@@ -34,17 +41,18 @@ class NewVisitorTest(unittest.TestCase):
         #他按回车键后，页面更新了
         #待办事项表格中显示了”1：买一本书“
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1:买一本书')
        
-        table = self.brower.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+#        table = self.brower.find_element_by_id('id_list_table')
+#        rows = table.find_elements_by_tag_name('tr')
 #        self.assertTrue(any(row.text == '1:买一本书' for row in rows),
 #                        "New to-do did not appear in table -- its text was:\n%s" %(
 #                        table.text,
 #                        ))
-        self.assertIn('1:买一本书',[row.text for row in rows],
-                        "New to-do did not appear in table -- its text was:\n%s" %(
-                        table.text,
-                        ))
+#        self.assertIn('1:买一本书',[row.text for row in rows],
+#                        "New to-do did not appear in table -- its text was:\n%s" %(
+#                        table.text,
+#                        ))
 
         #页面中又显示了一个文本框，可以输入其他待办事项
         #他又输入了”看书后思考“
@@ -52,16 +60,18 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys("看书后思考")
         inputbox.send_keys(Keys.ENTER)
         #页面再次更新，他的清单中显示了两个待办事项
-        table = self.brower.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:买一本书',[row.text for row in rows],
-                        "New to-do did not appear in table -- its text was:\n%s" %(
-                        table.text,
-                        ))        
-        self.assertIn('2:看书后思考',[row.text for row in rows],
-                        "New to-do did not appear in table -- its text was:\n%s" %(
-                        table.text,
-                        ))
+        self.check_for_row_in_list_table("1:买一本书")
+        self.check_for_row_in_list_table("2:看书后思考")
+#        table = self.brower.find_element_by_id('id_list_table')
+#        rows = table.find_elements_by_tag_name('tr')
+#        self.assertIn('1:买一本书',[row.text for row in rows],
+#                        "New to-do did not appear in table -- its text was:\n%s" %(
+#                        table.text,
+#                        ))        
+#        self.assertIn('2:看书后思考',[row.text for row in rows],
+#                        "New to-do did not appear in table -- its text was:\n%s" %(
+#                        table.text,
+#                        ))
         #老王想知道这个网站是否会记住他的清单
         #他看到利网站为他生成了一个唯一的URL
         #而且页面中有一些文字解说这个功能
