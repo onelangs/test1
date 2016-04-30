316c1ae0811178178af315072af0907e475d6533
onelangs <onelang@163.com>
2016-04-30 10:08:27 +0800

    functional test 增加对录入待办事项功能的测试

--- func_test.py
+++ func_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 #-*- coding:utf8 _*_
 from selenium import webdriver
+from selenium.webdriver.common import keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -18,15 +19,24 @@ class NewVisitorTest(unittest.TestCase):
         #他去看了这个应用的首页
         self.brower.get('http://localhost:8000')
         #他注意到网页的标题盒头部都包含“To-Do”这个词
-        self.assertIn('To-Do list',self.brower.title)
-        self.fail('finish the test!')
+        self.assertIn('To-Do',self.brower.title)
+        header_text = self.brower.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
         
         
         #应用邀请他输入一个待办事项
+        inputbox = self.brower.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+
         #他在一个文本框中输入了"买一本书"
         #老王爱看书
+        inputbox.send_keys('买一本书')
         #他按回车键后，页面更新了
         #待办事项表格中显示了”1：买一本书“
+        inputbox.send_keys(keys.ENTER)
+        table = self.brower.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1:买一本书' for row in rows))
         
         #页面中又显示利一个文本框，可以输入其他待办事项
         #他又输入了”看书后思考“
@@ -38,6 +48,8 @@ class NewVisitorTest(unittest.TestCase):
         #而且页面中有一些文字解说这个功能
         #他访问这个URL，发现他的待办事项还在。
         #老王很满意，去吃饭利。
+        self.fail('finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main()
--- test1/urls.py
+++ test1/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'test1.views.home', name='home'),
+    #url(r'^$', 'test1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$','lists.views.home_page',name='home'),
     url(r'^admin/', include(admin.site.urls)),
